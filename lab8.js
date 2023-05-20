4b5e5e198e580aa5748eba4116ff7a1abd8c333a
provorova <prlprovorova@yandex.ru>
2023-05-20 10:55:22 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -5,9 +5,26 @@ function showDate() {
     let out4 = document.getElementById ('current-date4');
     let out5 = document.getElementById ('current-date5');
     let today = new Date();
-    out1.innerHTML = 'Дата и время для русской локали: ' + today.toLocaleString('ru-RU'); 
+    out1.innerHTML ='Дата и время для русской локали: ' + today.toLocaleString('ru-RU'); 
     out2.innerHTML ='Дата и время для испанской локали: ' + today.toLocaleString('es');
     out3.innerHTML ='Дата и время для арсмянской локали: ' + today.toLocaleString('hy') ;
     out4.innerHTML ='Дата и время для немецкой локали: ' + today.toLocaleString('de') ;
     out5.innerHTML ='Дата и время для японской локали: ' + today.toLocaleString('ja');
+}
+
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    let days = document.getElementById ('dd');
+    days.innerHTML = 'Количество дней с даты рождения: ' + daysCount;
+}
+
+function clearDate() {
+    let inputDate = document.querySelector('input[type=date]');
+    inputDate.value = '';
+    let days = document.getElementById ('dd');
+    days.innerHTML = '';
 }
